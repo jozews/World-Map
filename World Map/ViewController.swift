27dcb0052059d2e9481a4c5276b3e600c6dfa1cd
Jože Ws <joze.ws11@gmail.com>
2016-08-26 17:03:36 -0500

    Initial Commit

---
+++ World Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  World Map
+//
+//  Created by Jože Ws on 8/26/16.
+//  Copyright © 2016 JožeWs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
