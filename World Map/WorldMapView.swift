0fe66c2a379b844b817a36cfb25f30b44d25a3a4
Jože Ws <joze.ws11@gmail.com>
2016-08-27 01:02:25 -0500

    first commit

---
+++ World Map/WorldMapView.swift
@@ -0,0 +1,13 @@
+//
+//  WorldMapView.swift
+//  World Map
+//
+//  Created by Jože Ws on 8/26/16.
+//  Copyright © 2016 JožeWs. All rights reserved.
+//
+
+import Cocoa
+
+class WorldMapView: UIView {
+
+}
